5a7125ac2504e32205770190113d312af5961a9e
HChan <hchanenator@gmail.com>
2018-12-29 17:59:36 -0500

    Initial Commit

---
+++ FlipIt/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlipIt
+//
+//  Created by Herb on 2018-12-29.
+//  Copyright © 2018 Herb. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ FlipItTests/FlipItTests.swift
@@ -0,0 +1,34 @@
+//
+//  FlipItTests.swift
+//  FlipItTests
+//
+//  Created by Herb on 2018-12-29.
+//  Copyright © 2018 Herb. All rights reserved.
+//
+
+import XCTest
+@testable import FlipIt
+
+class FlipItTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
