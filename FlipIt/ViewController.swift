d6ecf57012daf176b60a0294844c4b3a693cfd14
HChan <hchanenator@gmail.com>
2018-12-31 16:45:29 -0500

    Add .gitignore and change storyboard

--- FlipIt/ViewController.swift
+++ FlipIt/ViewController.swift
@@ -10,10 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var thePic: UIButton!
+    
+    let notGrandme:UIImage = UIImage(named: "NotGrandma")!
+    let notAFart:UIImage = UIImage(named: "NotAFart")!
+    let notApproved:UIImage = UIImage(named: "Disapproval")!
+    
+    var counter: Int = 0;
+    
+    
+    @IBAction func DaBut(_ sender: Any) {
+        counter = counter + 1;
+        
+        if (counter%3 == 1) {
+            thePic.setImage(notGrandme, for: .normal)
+        } else if (counter%3 == 2) {
+            thePic.setImage(notAFart, for: .normal)
+        } else {
+            thePic.setImage(notApproved, for: .normal)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        thePic.setImage(notGrandme, for: .normal)
+        
     }
+    
 
 
 }
